62095ecaa8226fcaadd0a79287830d685d7d6e96
maurice-cubicit <maurice@cubicit.co.za>
2023-11-15 23:50:37 +0200

    Initial commit

--- src/components/navigation/BottomNav.tsx
+++ src/components/navigation/BottomNav.tsx
@@ -10,11 +10,6 @@ function BottomNav() {
     <div className='w-10/12 bg-gray-100 flex m-auto'>
         <div className='w-9/12 py-8 flex justify-between px-5'>
             <Button title={'Test'} />
-            <Button title={'Test'} />
-            <Button title={'Test'} />
-            <Button title={'Test'} />
-            <Button title={'Test'} />
-            <Button title={'Test'} />
         </div>
         <div className='w-3/12 bg-gray-300 py-8 h-96'>
             test 2
