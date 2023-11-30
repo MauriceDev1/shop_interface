c3cda67f2132d44e19df52d35977b2a48d3c1d69
maurice-cubicit <maurice@cubicit.co.za>
2023-11-30 21:32:15 +0200

    fix: added link as well as tabs

--- src/components/IconBox.tsx
+++ src/components/IconBox.tsx
@@ -4,22 +4,26 @@ interface IconBoxProps {
   color: string,
   icon: any,
   label: string,
+  href: string,
+  tabs: number,
 }
 
-const IconBox = ({color, icon, label} : IconBoxProps) => {
+const IconBox = ({color, icon, label, href, tabs} : IconBoxProps) => {
   return (
-    <div className="min-w-[160px] w-1/6 full p-2">
-      <div className="w-full flex h-28 rounded" style={{backgroundColor:`${color}`}}>
-        <div className="m-auto">
-          {icon}
+    <a href='https://google.com' target={tabs === 1 ?'_self' : '_blank'} rel="noreferrer">
+      <div className="min-w-[160px] w-1/6 full p-2">
+        <div className="w-full flex h-28 rounded" style={{backgroundColor:`${color}`}}>
+          <div className="m-auto">
+            {icon}
+          </div>
+        </div>
+        <div>
+          <p className="text-center mt-3">
+            {label}
+          </p>
         </div>
       </div>
-      <div>
-        <p className="text-center mt-3">
-          {label}
-        </p>
-      </div>
-    </div>
+    </a>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,48 +7,62 @@ function Home() {
       id: 1,
       color: "#b6b690",
       icon: <AccessTime24Regular className="text-white w-12 h-12"/>,
-      label: "KPI's"
+      label: "KPI's",
+      href: "https://google.com",
+      tabs: 1
     },
     {
       id: 2,
       color: "#debfc6",
       icon: <MailRead24Regular className="text-white w-12 h-12"/>,
-      label: "Dayforce"
+      label: "Dayforce",
+      href: "https://facebook.com",
+      tabs: 2
     },
     {
       id: 3,
       color: "#d29f91",
       icon: <Mail24Regular className="text-white w-12 h-12"/>,
-      label: "Email"
+      label: "Email",
+      href: "https://firefox.com",
+      tabs: 1
     },
     {
       id: 4,
       color: "#b6b690",
       icon: <Box24Regular className="text-white w-12 h-12"/>,
-      label: "OneStock"
+      label: "OneStock",
+      href: "https://twitter.com",
+      tabs: 2
     },
     {
       id: 5,
       color: "#debfc6",
       icon: <ShareScreenPerson24Regular className="text-white w-12 h-12"/>,
-      label: "Fuse/HR"
+      label: "Fuse/HR",
+      href: "https://gmail.com",
+      tabs: 1
     },
     {
       id: 6,
       color: "#d29f91",
       icon: <TaskListSquareAdd24Regular className="text-white w-12 h-12"/>,
-      label: "Yoobick"
+      label: "Yoobick",
+      href: "https://google.com",
+      tabs: 1
     }
   ]
   return (
       <div className="w-full py-5">
-        <div className="w-8/12 py-2 m-auto justify-center flex flex-wrap">
+        <div className="w-8/12 py-2 border-t border-b m-auto justify-center flex flex-wrap">
           {data.map((d) => 
             <IconBox 
               key={d.id}
               color={d.color}
               icon={d.icon}
               label={d.label}
+              href={d.href}
+              tabs={d.tabs}
             />
           )}
         </div>  
